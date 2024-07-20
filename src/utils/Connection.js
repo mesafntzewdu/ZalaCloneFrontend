e43a68d9e36bfd582bfbc55201f8d55fb03d1644
Hilcbek <balemayehu07@gmail.com>
2024-07-20 18:52:33 +0300

    added final part 2

--- src/utils/Connection.js
+++ src/utils/Connection.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseURL = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://zala-back.onrender.com/api',
   withCredentials: true,
 });
 //https://zala-back.onrender.com
